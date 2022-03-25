12aa2aaa47dd4dc022a9685188a79106e9189e14
Lightnet <darkneter@gmail.com>
2022-03-25 08:59:26 -0700

    update.

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   <div>
     <router-link to="/">Home</router-link><span> | </span>
     <router-link to="/about">About</router-link><span> | </span>
-    <router-link to="/editor">Editor</router-link><span> | </span>
+    <router-link to="/errorpage">error page</router-link><span> | </span>
     <router-view v-slot="{ Component }">
       <Suspense>
         <component :is="Component" />
